0d08a93674c24fdb5fc0ae43f4d898e8925066c1
DorianWlodarczyk <99553002+DorianWlodarczyk@users.noreply.github.com>
2022-12-11 11:21:59 +0100

    poprawiony program z odpowiednią konwersją stringów na inty

--- lab6/main.py
+++ lab6/main.py
@@ -9,9 +9,14 @@ def my_printf(format_string,param):
         if shouldDo:
             if format_string[idx] == '#' and format_string[idx+1] == '.' and (format_string[idx+2] == 'X' or format_string[idx+2] == 'x') and (format_string[idx+3] == 'g' or format_string[idx+3] == 'G'):
                 for j in range(0,len(format_string)):
-                    oldNumber = format_string[j]
-                    newNumber = ((oldNumber*9+1)%10)
-                print(newNumber,end="")
+                    if format_string[j] >= '0' and format_string[j] <= '9':
+                        oldNumber = format_string[j]
+                        oldNumberToInt = int(oldNumber)
+                        newNumber = ((oldNumberToInt*9+1)%10)
+                        newNumberToString = str(newNumber)
+                        print(newNumber,end="")
+                    else:
+                        print(format_string[j], end="")
                 shouldDo=False
             else:
                 print(format_string[idx],end="")
@@ -23,4 +28,3 @@ data=sys.stdin.readlines()
 
 for i in range(0,len(data),2):
     my_printf(data[i].rstrip(),data[i+1].rstrip())
-

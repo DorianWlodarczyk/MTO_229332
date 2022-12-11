9a0323437d4eb459217c4849047302e449988e53
DorianWlodarczyk <99553002+DorianWlodarczyk@users.noreply.github.com>
2022-12-11 11:04:24 +0100

    Dodano obsługe małej i dużej litery x oraz g

--- lab6/main.py
+++ lab6/main.py
@@ -7,11 +7,11 @@ def my_printf(format_string,param):
     shouldDo=True
     for idx in range(0,len(format_string)):
         if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == '.' and format_string[idx+2] == 'X' and format_string[idx+3] == 'g':
+            if format_string[idx] == '#' and format_string[idx+1] == '.' and (format_string[idx+2] == 'X' or format_string[idx+2] == 'x') and (format_string[idx+3] == 'g' or format_string[idx+3] == 'G'):
                 for j in range(0,len(format_string)):
                     oldNumber = format_string[j]
                     newNumber = ((oldNumber*9+1)%10)
-                print(param,end="")
+                print(newNumber,end="")
                 shouldDo=False
             else:
                 print(format_string[idx],end="")
@@ -23,3 +23,4 @@ data=sys.stdin.readlines()
 
 for i in range(0,len(data),2):
     my_printf(data[i].rstrip(),data[i+1].rstrip())
+
